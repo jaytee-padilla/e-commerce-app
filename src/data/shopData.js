dac9d5cb8a298bb61bd96c77ced4b2433e22cb7f
Jaytee <Padilla.Jaytee@gmail.com>
2020-07-15 14:37:04 -0700

    Created a file containing product data for the shop part of the app

---
+++ src/data/shopData.js
@@ -0,0 +1,266 @@
+const SHOP_DATA = [
+  {
+    id: 1,
+    title: 'New Arrivals',
+    routeName: 'new-arrivals',
+    items: [
+      {
+        id: 1,
+        name: 'Max Allure Jonate',
+        imgURL: 'https://i.imgur.com/YqxisfQ.jpg',
+        price: 50
+      },
+      {
+        id: 2,
+        name: 'Max Allure Dre',
+        imgURL: 'https://i.imgur.com/xBpl9Fi.jpg',
+        price: 50
+      },
+      {
+        id: 3,
+        name: 'Atlas Tee Navy',
+        imgURL: 'https://i.imgur.com/tpx12ks.jpg',
+        price: 25
+      },
+      {
+        id: 4,
+        name: 'Atlas Tee Red',
+        imgURL: 'https://i.imgur.com/imbpgK4.jpg',
+        price: 25
+      },
+      {
+        id: 5,
+        name: 'Nike Blazer',
+        imgURL: 'https://i.imgur.com/bg088LA.jpg',
+        price: 60
+      },
+      {
+        id: 6,
+        name: 'Nike Shane',
+        imgURL: 'https://i.imgur.com/qGa5cyi.jpg',
+        price: 60
+      },
+      {
+        id: 7,
+        name: 'Spitfire Wheel Red',
+        imgURL: 'https://i.imgur.com/LG5Jszs.jpg',
+        price: 30
+      },
+      {
+        id: 8,
+        name: 'Spitfire Wheel Blue',
+        imgURL: 'https://i.imgur.com/tfUWKtx.jpg',
+        price: 30
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: 'Skateboarding',
+    routeName: 'skateboarding',
+    items: [
+      {
+        id: 1,
+        name: 'Magenta Blue',
+        imgURL: 'https://i.imgur.com/7oL8App.jpg',
+        price: 50
+      },
+      {
+        id: 2,
+        name: 'Magenta Green',
+        imgURL: 'https://i.imgur.com/m9d1BEP.jpg',
+        price: 50
+      },
+      {
+        id: 3,
+        name: 'Habitat',
+        imgURL: 'https://i.imgur.com/Aw92HXd.jpg',
+        price: 50
+      },
+      {
+        id: 4,
+        name: 'Alien Blue',
+        imgURL: 'https://i.imgur.com/MXxSmoJ.jpg',
+        price: 50
+      },
+      {
+        id: 5,
+        name: 'Alien Green',
+        imgURL: 'https://i.imgur.com/47dl7Oo.jpg',
+        price: 50
+      },
+      {
+        id: 6,
+        name: 'Max Allure Jonate',
+        imgURL: 'https://i.imgur.com/YqxisfQ.jpg',
+        price: 50
+      },
+      {
+        id: 7,
+        name: 'Max Allure Dre',
+        imgURL: 'https://i.imgur.com/xBpl9Fi.jpg',
+        price: 50
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: 'Footwear',
+    routeName: 'footwear',
+    items: [
+      {
+        id: 1,
+        name: 'Nike Blazer',
+        imgURL: 'https://i.imgur.com/bg088LA.jpg',
+        price: 60
+      },
+      {
+        id: 2,
+        name: 'Nike Shane',
+        imgURL: 'https://i.imgur.com/qGa5cyi.jpg',
+        price: 60
+      },
+      {
+        id: 3,
+        name: 'Nike Bruin',
+        imgURL: 'https://i.imgur.com/7UzZvNT.jpg',
+        price: 60
+      },
+      {
+        id: 4,
+        name: 'Cons CTS',
+        imgURL: 'https://i.imgur.com/6jwzmEX.jpg',
+        price: 60
+      },
+      {
+        id: 5,
+        name: 'Cons Jack Purcell',
+        imgURL: 'https://i.imgur.com/XRQle54.jpg',
+        price: 60
+      },
+      {
+        id: 6,
+        name: 'Vans Old Skool',
+        imgURL: 'https://i.imgur.com/Xc5KJWz.jpg',
+        price: 60
+      },
+      {
+        id: 7,
+        name: 'Lakai Sheffield',
+        imgURL: 'https://i.imgur.com/03u7rmn.jpg',
+        price: 60
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: 'Clothing',
+    routeName: 'clothing',
+    items: [
+      {
+        id: 1,
+        name: 'Atlas Tee Navy',
+        imgURL: 'https://i.imgur.com/tpx12ks.jpg',
+        price: 25
+      },
+      {
+        id: 2,
+        name: 'Atlas Tee Red',
+        imgURL: 'https://i.imgur.com/imbpgK4.jpg',
+        price: 25
+      },
+      {
+        id: 3,
+        name: 'Dime Yellow',
+        imgURL: 'https://i.imgur.com/4uAsW5v.jpg',
+        price: 80
+      },
+      {
+        id: 4,
+        name: 'Dime Black',
+        imgURL: 'https://i.imgur.com/Ie4KxQx.jpg',
+        price: 80
+      },
+      {
+        id: 5,
+        name: 'Hotel Blue Green',
+        imgURL: 'https://i.imgur.com/11elXMG.jpg',
+        price: 25
+      },
+      {
+        id: 6,
+        name: 'Hotel Blue Black',
+        imgURL: 'https://i.imgur.com/7sDb3S9.jpg',
+        price: 40
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: 'Accessories',
+    routeName: 'accessories',
+    items: [
+      {
+        id: 1,
+        name: 'Spitfire Wheel Red',
+        imgURL: 'https://i.imgur.com/LG5Jszs.jpg',
+        price: 30
+      },
+      {
+        id: 2,
+        name: 'Spitfire Wheel Blue',
+        imgURL: 'https://i.imgur.com/tfUWKtx.jpg',
+        price: 30
+      },
+      {
+        id: 3,
+        name: 'Quarter Snacks Fanny Pack',
+        imgURL: 'https://i.imgur.com/HTyAmlA.jpg',
+        price: 30
+      },
+      {
+        id: 4,
+        name: 'Magenta Satchel Burgundy',
+        imgURL: 'https://i.imgur.com/6YyKz6I.jpg',
+        price: 30
+      },
+      {
+        id: 5,
+        name: 'Magenta Satchel White',
+        imgURL: 'https://i.imgur.com/45RVua8.jpg',
+        price: 30
+      },
+      {
+        id: 6,
+        name: 'Atlas Pen White',
+        imgURL: 'https://i.imgur.com/ZmQfD4O.jpg',
+        price: 10
+      },
+      {
+        id: 7,
+        name: 'Atlas Pen Black',
+        imgURL: 'https://i.imgur.com/IGxhC1q.jpg',
+        price: 10
+      }
+    ]
+  }
+];
+
+export default SHOP_DATA;
+
+{
+  id: ,
+  name: '',
+  imgURL: '',
+  price: 
+},
+
+{
+  id: ,
+  title: '',
+  routeName: '',
+  items: [
+
+  ]
+},
+
