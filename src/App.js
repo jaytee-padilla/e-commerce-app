553eb8eeaef508d523efbbbcf8a8bee57ba99382
Jaytee <Padilla.Jaytee@gmail.com>
2020-07-14 19:37:20 -0700

    Installed and began implementation of react-router. Attached homepage to route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 // components
 import HomePage from './pages/homepage/homepage';
 
+const FootwearPage = () => (
+  <div>
+    <h1>Footwear Page</h1>
+  </div>
+)
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/footwear' component={FootwearPage} />
+      </Switch>
     </div>
   );
 }
