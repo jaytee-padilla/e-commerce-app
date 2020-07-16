cccc1d04f95a7bb505385af0c1cea43f26e3780b
Jaytee <Padilla.Jaytee@gmail.com>
2020-07-16 14:06:44 -0700

    Created a sign in/sign up page component. Created a sign-in component where a user will be able to enter their login info

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
+import SignInAndSignUp from './pages/signin-signup/signin-signup';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/pages/signin-signup/signin-signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './signin-signup.scss';
+
+// components
+import SignIn from '../../components/sign-in/sign-in';
+
+const SignInAndSignUp = () => {
+  return (
+    <div className='sign-in-sign-up'>
+      <SignIn />
+    </div>
+  )
+}
+
+export default SignInAndSignUp;
+
