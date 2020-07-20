ac8f4f219674d75a23f46deaef8119ca7f8f503e
Jaytee <Padilla.Jaytee@gmail.com>
2020-07-20 14:03:58 -0700

    Implemented sign-in authentication state tracking at the App component level

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -7,18 +7,47 @@ import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
 import Header from './components/header/header';
 import SignInAndSignUp from './pages/signin-signup/signin-signup';
+import { auth } from './firebase/firebase';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+  
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    // this listens for authentication state changes on the firebase backend
+    // when a user signs in, the onAuthStateChanged method records info about the user logging in or out
+    // that "user" data is then being stored in the App components state for authentication use throughout the app
+    // by using this method inside of a componentDidMount() function at the App component level, the sign-in status of the current user is consistently tracked during app usage
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+    });
+  }
+
+  componentWillUnmount() {
+    // this helps prevent memory leaks in the application
+    // when the App component unmounts (signing out, switching browser tabs, closing the browser, etc.), this closes the communication between the app itself and the firebase app
+    this.unsubscribeFromAuth();
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
