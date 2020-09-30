8a154a6dedcfdb9362104b058943649792c9b0b7
Jaytee <Padilla.Jaytee@gmail.com>
2020-09-29 21:00:17 -0700

    Added custom button to CollectionItem component and implemented hover mouse effect

--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './collection-item.scss';
 
+// components
+import CustomButton from '../custom-button/custom-button';
+
 
 function CollectionItem({id, name, imgURL, price}) {
   return (
@@ -16,6 +19,8 @@ function CollectionItem({id, name, imgURL, price}) {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
+
+      <CustomButton inverted>Add To Cart</CustomButton>
     </div>
   )
 }
